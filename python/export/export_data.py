8f020f3ec48070fd9aceeea57379376386c12322
mantmic <michael.f.mantfeld@gmail.com>
2020-06-13 08:43:26 +1000

    Added table export to cicd pipeline

---
+++ python/export/export_data.py
@@ -0,0 +1,42 @@
+import os
+from google.cloud import bigquery
+
+# get environment variables
+project = os.getenv('GCP_PROJECT_ID', "melbourne-gig-guide")
+location = os.getenv('GCP_LOCATION', "australia-southeast1")
+dataset_id = os.getenv('BIGQUERY_DATASET_ID', "dev")
+bucket_name = os.getenv('GCP_BUCKET_NAME', "melbourne-gig-guide-public-dev")
+
+# initialize client
+client = bigquery.Client()
+
+# list of tables to export
+table_export_list = ['venue']
+
+for table_id in table_export_list:
+
+    destination_uri = "gs://{}/{}".format(bucket_name, ".".join([table_id,'csv']))
+
+    print(
+        "Exporting {}:{}.{} to {}".format(project, dataset_id, table_id, destination_uri)
+    )
+    dataset_ref = bigquery.DatasetReference(project, dataset_id)
+    table_ref = dataset_ref.table(table_id)
+
+    # define job config
+    job_config = bigquery.ExtractJobConfig()
+    job_config.destination_format = (bigquery.DestinationFormat.CSV)
+
+    # extract data
+    extract_job = client.extract_table(
+        table_ref,
+        destination_uri,
+        # Location must match that of the source table.
+        location=location,
+        job_config=job_config
+    )  # API request
+    extract_job.result()  # Waits for job to complete.
+
+    print(
+        "Exported {}:{}.{} to {}".format(project, dataset_id, table_id, destination_uri)
+    )
---
+++ python/extract/get_datamelbourne.py
@@ -0,0 +1,19 @@
+import requests
+from google.cloud import bigquery
+import config
+bigquery_client = bigquery.Client()
+dataset = bigquery_client.dataset(config.bigquery_dataset_id)
+
+table = dataset.table('datamelbourne_music_venue')
+
+url = "https://data.melbourne.vic.gov.au/resource/mgqj-necz.json"
+
+res = requests.get(url)
+
+data = res.json()
+job_config = bigquery.LoadJobConfig()
+job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+
+job = bigquery_client.load_table_from_json(data, table, job_config = job_config)
+
+print(job.result())
