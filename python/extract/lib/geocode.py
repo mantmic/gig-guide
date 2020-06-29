0583074565f05c55f185ff345b8edb368ec3f335
mantmic <michael.f.mantfeld@gmail.com>
2020-06-30 06:59:12 +1000

    Fixed geocoding table

--- python/extract/lib/geocode.py
+++ python/extract/lib/geocode.py
@@ -3,12 +3,12 @@ import datetime
 
 def get_geocode(input_address):
     g = geocoder.google(input_address)
-    print(g.json)
-    print(g)
+    extract_keys = ['address','city','country','lat','lng','bbox']
+    json_result = {key:g.json.get(key) for key in extract_keys}
     return({
         'input_address':input_address,
         'geocode_provider':g.provider,
         'extract_ts':datetime.datetime.now().isoformat(),
         #'geojson_result':g.geojson,
-        'json_result':g.json
+        'json_result':json_result
     })
