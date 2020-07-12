dc1d0a0b69f9f3b4f1b8addebf76bf7c8a8fdbb6
mantmic <michael.f.mantfeld@gmail.com>
2020-07-12 12:34:31 +1000

    Added reverbnation base dbt model

---
+++ sql/models/reverbnation/base/reverbnation_venue_shows.sql
@@ -0,0 +1,13 @@
+{{ config(materialized='view') }}
+
+select
+    to_hex ( {{ dbt_utils.surrogate_key('venue_reverbnation_url') }} )  as reverbnation_venue_id 
+  , venue_reverbnation_url  
+  , trim ( venue_name )                                                 as venue_name 
+  , venue_location_full 
+  , venue_image_url 
+  , venue_social_links
+  , shows
+  , row_number() over ( partition by venue_reverbnation_url order by extract_ts desc ) as dup_rn 
+from  
+  {{ source('reverbnation', 'reverbnation_venue_shows') }}
