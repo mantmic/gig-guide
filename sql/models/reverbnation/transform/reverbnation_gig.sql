6047f1edbefc409c84c1f855da042a1f8ec108f4
mantmic <michael.f.mantfeld@gmail.com>
2020-07-16 18:01:23 +1000

    Updated reverbnation gig datetime logic

--- sql/models/reverbnation/transform/reverbnation_gig.sql
+++ sql/models/reverbnation/transform/reverbnation_gig.sql
@@ -2,12 +2,12 @@
 
 with dup_data as 
 ( select
-    to_hex ( {{ dbt_utils.surrogate_key('venue_shows.show_reverbnation_url') }} )   as reverbnation_gig_id 
+    to_hex ( {{ dbt_utils.surrogate_key('venue_shows.show_reverbnation_url') }} )     as reverbnation_gig_id 
   , venue.reverbnation_venue_id
-  , venue_shows.show_reverbnation_url                                               as reverbnation_url
-  , {{ clean_url('venue_shows.show_ticket_url') }}                                  as ticket_url
-  , datetime ( venue_shows.show_datetime, 'Australia/Melbourne' )                   as gig_datetime
-  , cast ( datetime (venue_shows.show_datetime, 'Australia/Melbourne' ) as date )   as gig_date
+  , venue_shows.show_reverbnation_url                                                 as reverbnation_url
+  , {{ clean_url('venue_shows.show_ticket_url') }}                                    as ticket_url
+  -- undo the -4 applied to the raw timestamp 
+  , datetime_add ( datetime ( venue_shows.show_datetime, 'UTC' ), interval -4 hour )  as gig_datetime
   , venue_shows.show_artists
   , row_number() over ( partition by venue_shows.show_reverbnation_url order by venue.extract_ts desc ) as dup_rn
 from 
@@ -16,7 +16,8 @@ from
   unnest ( venue.shows ) venue_shows 
 ) 
 select
-  *
+    *
+  , cast ( gig_datetime as date ) as gig_date 
 from 
   dup_data
 where
